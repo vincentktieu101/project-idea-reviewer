f4cffd8985cab1b0c204c2ab49068d6a0755ae4c
Phillip Conrad <pconrad@Phillips-MacBook-Pro.local>
2020-03-30 09:54:19 -0700

    pc - add table for reviews

---
+++ src/main/java/edu/ucsb/cs48/s20/demo/entities/Review.java
@@ -0,0 +1,104 @@
+package edu.ucsb.cs48.s20.demo.entities;
+
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+   
+    @OneToOne
+    @JoinColumn(name = "student_id")
+    private Student reviewer;
+    
+    @OneToOne
+    @JoinColumn(name = "idea_id")
+    private ProjectIdea idea;
+    
+    private Integer rating;
+
+    @Column(columnDefinition = "LONGTEXT")
+    private String details;
+
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Student getReviewer() {
+        return this.reviewer;
+    }
+
+    public void setReviewer(Student reviewer) {
+        this.reviewer = reviewer;
+    }
+
+    public ProjectIdea getIdea() {
+        return this.idea;
+    }
+
+    public void setIdea(ProjectIdea idea) {
+        this.idea = idea;
+    }
+
+    public Integer getRating() {
+        return this.rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    public String getDetails() {
+        return this.details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+    
+
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", reviewer='" + getReviewer() + "'" +
+            ", idea='" + getIdea() + "'" +
+            ", rating='" + getRating() + "'" +
+            ", details='" + getDetails() + "'" +
+            "}";
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Review)) {
+            return false;
+        }
+        Review review = (Review) o;
+        return id == review.id && Objects.equals(reviewer, review.reviewer) && Objects.equals(idea, review.idea) && Objects.equals(rating, review.rating) && Objects.equals(details, review.details);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, reviewer, idea, rating, details);
+    }
+
+
+}
