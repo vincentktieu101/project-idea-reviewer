f3d079ac23b17e31f0ea5e8c59d77c7f25ada791
Scott Chow <tehchowster@gmail.com>
2020-03-31 14:03:28 -0700

    sc - add # reviews and avg rating to ideas index

--- src/main/java/edu/ucsb/cs48/s20/demo/entities/ProjectIdea.java
+++ src/main/java/edu/ucsb/cs48/s20/demo/entities/ProjectIdea.java
@@ -21,92 +21,99 @@ import javax.validation.constraints.NotBlank;
 @Entity
 public class ProjectIdea {
 
-    public static final int TITLE_CHAR_MIN=4;
-	public static final int TITLE_CHAR_MAX=60;
+	public static final int TITLE_CHAR_MIN = 4;
+	public static final int TITLE_CHAR_MAX = 60;
 
-	public static final int DETAILS_CHAR_MIN=30;
-	public static final int DETAILS_CHAR_MAX=255;
+	public static final int DETAILS_CHAR_MIN = 30;
+	public static final int DETAILS_CHAR_MAX = 255;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
 
-    @OneToOne(mappedBy = "projectIdea", cascade = CascadeType.REFRESH)
-    @JoinColumn(name = "student_id")
-    private Student student;
+	@OneToOne(mappedBy = "projectIdea", cascade = CascadeType.REFRESH)
+	@JoinColumn(name = "student_id")
+	private Student student;
 
-    @NotBlank
-    private String title;
+	@NotBlank
+	private String title;
 
-    @NotBlank
-    @Column(columnDefinition = "text")
-    private String details;
+	@NotBlank
+	@Column(columnDefinition = "text")
+	private String details;
 
-    @OneToMany(mappedBy = "idea", cascade = CascadeType.ALL)
-    private Set<Review> reviews;
+	@OneToMany(mappedBy = "idea", cascade = CascadeType.ALL)
+	private Set<Review> reviews;
 
-    @PreRemove
-    private void preRemove() {
-        student.setProjectIdea(null);
-    }
+	@PreRemove
+	private void preRemove() {
+		student.setProjectIdea(null);
+	}
 
-    public long getId() {
-        return this.id;
-    }
+	public long getId() {
+		return this.id;
+	}
 
-    public void setId(long id) {
-        this.id = id;
-    }
+	public void setId(long id) {
+		this.id = id;
+	}
 
-    public Student getStudent() {
-        return this.student;
-    }
+	public Student getStudent() {
+		return this.student;
+	}
 
-    public void setStudent(Student student) {
-        this.student = student;
-    }
+	public void setStudent(Student student) {
+		this.student = student;
+	}
 
-    public String getTitle() {
-        return this.title;
-    }
+	public String getTitle() {
+		return this.title;
+	}
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-    public String getDetails() {
-        return this.details;
-    }
+	public String getDetails() {
+		return this.details;
+	}
 
-    public void setDetails(String details) {
-        this.details = details;
-    }
+	public void setDetails(String details) {
+		this.details = details;
+	}
 
-    public Set<Review> getReviews() {
-        return this.reviews;
-    }
+	public Set<Review> getReviews() {
+		return this.reviews;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (o == this)
-            return true;
-        if (!(o instanceof ProjectIdea)) {
-            return false;
-        }
-        ProjectIdea projectIdea = (ProjectIdea) o;
-        return id == projectIdea.id && Objects.equals(student, projectIdea.student)
-                && Objects.equals(title, projectIdea.title) && Objects.equals(details, projectIdea.details);
-    }
+	public Double getAvgRating() {
+		if (reviews.isEmpty())
+			return 0.0;
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, student, title, details);
-    }
+		return reviews.stream().mapToInt((review) -> review.getRating()).average().orElse(0.0);
+	}
 
-    @Override
-    public String toString() {
-        return "{" + " id='" + getId() + "'" + ", student='" + getStudent() + "'" + ", title='" + getTitle() + "'"
-                + ", details='" + getDetails() + "'" + "}";
-    }
+	@Override
+	public boolean equals(Object o) {
+		if (o == this)
+			return true;
+		if (!(o instanceof ProjectIdea)) {
+			return false;
+		}
+		ProjectIdea projectIdea = (ProjectIdea) o;
+		return id == projectIdea.id && Objects.equals(student, projectIdea.student)
+				&& Objects.equals(title, projectIdea.title) && Objects.equals(details, projectIdea.details);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, student, title, details);
+	}
+
+	@Override
+	public String toString() {
+		return "{" + " id='" + getId() + "'" + ", student='" + getStudent() + "'" + ", title='" + getTitle() + "'"
+				+ ", details='" + getDetails() + "'" + "}";
+	}
 
 }
