e3ec25d4028994f5faf252c2ba8c0e6ae6219030
Jacqueline Mai <jacquelinemai@umail.ucsb.edu>
2020-04-08 17:44:39 -0700

    jm - added new mocks and controller setup to get first oauth test to pass

--- src/test/java/edu/ucsb/cs48/s20/demo/controllers/OAuthTest.java
+++ src/test/java/edu/ucsb/cs48/s20/demo/controllers/OAuthTest.java
@@ -1,5 +1,7 @@
 package edu.ucsb.cs48.s20.demo.controllers;
 
+import edu.ucsb.cs48.s20.demo.advice.AuthControllerAdvice;
+import edu.ucsb.cs48.s20.demo.advice.StudentFlowAdvice;
 import edu.ucsb.cs48.s20.demo.services.MembershipService;
 import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.BeforeAll;
@@ -7,9 +9,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.test.context.support.WithMockUser;
@@ -23,8 +29,8 @@ import org.junit.runner.RunWith;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
-@WebMvcTest(AdminController.class)
+// @RunWith(SpringRunner.class)
+@WebMvcTest(ApplicationController.class)
 public class OAuthTest {
 
     private OAuth2User principal;
@@ -35,8 +41,25 @@ public class OAuthTest {
     @Autowired
     private MockMvc mvc;
 
+    @MockBean
+    private StudentFlowAdvice sfa;
+    
+    @MockBean
+    private AuthControllerAdvice aca;
+
+    @MockBean
+    private ClientRegistrationRepository crr;
+
+    @Configuration
+    public static class TestConf {
+        @Bean
+        public ApplicationController applicationController() {
+            return new ApplicationController();
+        }
+    }
+
+    
     @Test
-    @WithMockUser
     public void testOauth() throws Exception {
         //OAuth2AuthenticationToken token =  OAuthUtils.getOauthAuthenticationFor(principal);
         principal = OAuthUtils.createOAuth2User("Chris Gaucho", "cgaucho@example.com");
