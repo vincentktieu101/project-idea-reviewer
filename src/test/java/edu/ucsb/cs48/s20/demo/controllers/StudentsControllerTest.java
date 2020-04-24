2abc3b17b728890361c2f0fb898e352c18a26af0
Cole Bergmann <43765292+colebergmann@users.noreply.github.com>
2020-04-23 21:13:27 -0700

    cb - fixed ci

--- src/test/java/edu/ucsb/cs48/s20/demo/controllers/StudentsControllerTest.java
+++ src/test/java/edu/ucsb/cs48/s20/demo/controllers/StudentsControllerTest.java
@@ -1,7 +1,11 @@
 package edu.ucsb.cs48.s20.demo.controllers;
 
+import edu.ucsb.cs48.s20.demo.advice.AuthControllerAdvice;
+import edu.ucsb.cs48.s20.demo.advice.StudentFlowAdvice;
 import edu.ucsb.cs48.s20.demo.entities.Student;
+import edu.ucsb.cs48.s20.demo.repositories.AppUserRepository;
 import edu.ucsb.cs48.s20.demo.repositories.StudentRepository;
+import edu.ucsb.cs48.s20.demo.services.GoogleMembershipService;
 import edu.ucsb.cs48.s20.demo.services.MembershipService;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,6 +19,7 @@ import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -46,14 +51,29 @@ public class StudentsControllerTest {
     @Autowired
     private MockMvc mvc;
 
-    @Autowired
-    private StudentsController studentsController;
+    @MockBean
+    private AuthControllerAdvice aca;
+
+    @MockBean
+    private ClientRegistrationRepository crr;
 
     @MockBean
     private MembershipService ms;
 
+    @Autowired
+    private StudentsController studentsController;
+
+    @MockBean
+    private GoogleMembershipService gms;
+
+    @MockBean
+    private AppUserRepository aur;
+
     @MockBean
     private StudentRepository sr;
+
+    @MockBean
+    private StudentFlowAdvice studentFlowAdvice;
 
     private Authentication mockAuthentication;
 
@@ -282,6 +302,4 @@ public class StudentsControllerTest {
         // Make sure no students were imported
         assert(captor.getValue().isEmpty());
     }
-
-
 }
