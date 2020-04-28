c0dfd5fa55a88c4f4b5dfc2ca32cecbe33da111f
Phillip Conrad <pconrad@Phillips-MacBook-Pro.local>
2020-04-28 15:07:49 -0700

    pc/sc - add command line runner for startup code

--- src/main/java/edu/ucsb/cs48/s20/demo/OurCommandLineRunner.java
+++ src/main/java/edu/ucsb/cs48/s20/demo/OurCommandLineRunner.java
@@ -1,5 +1,14 @@
 package edu.ucsb.cs48.s20.demo;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.util.List;
+import java.io.InputStreamReader;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,35 +16,90 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import edu.ucsb.cs48.s20.demo.entities.Student;
 import edu.ucsb.cs48.s20.demo.entities.Admin;
+
+import edu.ucsb.cs48.s20.demo.services.CSVToObjectService;
 import edu.ucsb.cs48.s20.demo.repositories.AdminRepository;
+import edu.ucsb.cs48.s20.demo.repositories.StudentRepository;
 
 @Component
 public class OurCommandLineRunner implements ApplicationRunner {
     private static final Logger logger = LoggerFactory.getLogger(OurCommandLineRunner.class);
 
     @Autowired
+    private StudentRepository studentRepository;
+
+    @Autowired
     private AdminRepository adminRepository;
+
+    @Autowired
+    CSVToObjectService<Student> csvToObjectService;
 
     // Note: you can pass commandline arguments to spring boot with the following
     // format:
-    // mvn spring-boot:run -Dspring-boot.run.arguments="--hello=world"
+    // mvn spring-boot:run -Dspring-boot.run.arguments="--load-students=filename"
+    // mvn spring-boot:run -Dspring-boot.run.arguments="--load-students=file1 --load-students=file2"
+    // mvn spring-boot:run -Dspring-boot.run.arguments="--add-admin=cgaucho@example.org"
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        logger.info("Running application with the following args: {}", args.getOptionNames());
-        for (String arg : args.getOptionNames()) {
-            logger.info("---- {} : {}", arg, args.getOptionValues(arg));
+
+        if (args.getOptionNames().size() == 0) {
+            logger.info("No command line arguments to OurCommandLineRunner");
+            return;
         }
+
+        if (args.containsOption("load-student-file")) {
+            loadStudentFiles(args.getOptionValues("load-student-file"));
+        }
+
+        if (args.containsOption("add-admin")) {
+            setAdmins(args.getOptionValues("add-admin"));
+        }
+
+    }
+
+    public void loadStudentFiles(List<String> filenames) {
+        for (var filename : filenames) {
+            loadStudentFile(filename);
+        }
+    }
+
+    public void setAdmins(List<String> admins) {
+        for (var admin : admins) {
+            setAdmin(admin);
+        }
+    }
+
+    public void loadStudentFile(String filename) {
+        logger.warn("Loading data from {}", filename);
+
+        InputStream targetStream = null;
+
+        try {
+            File initialFile = new File(filename);
+            targetStream = new FileInputStream(initialFile);
+        } catch (FileNotFoundException fnfe) {
+            logger.error("File not found: {}, no data loaded.", filename);
+            return;
+        }
+
+        try (Reader reader = new InputStreamReader(targetStream)) {
+            List<Student> students = csvToObjectService.parse(reader, Student.class);
+            studentRepository.saveAll(students);
+        } catch (IOException ioe) {
+            logger.error("Exception: ", ioe);
+        }
+
+    }
+
+    public void setAdmin(String adminEmail) {
+        logger.info("Adding admin {}", adminEmail);
         Admin newAdmin = new Admin();
-        newAdmin.setEmail("test@test.org");
+        newAdmin.setEmail(adminEmail);
         newAdmin = adminRepository.save(newAdmin);
-        for (Admin admin : adminRepository.findAll()) {
-            logger.info("---- {} : {}", admin.getId(), admin.getEmail());
-        }
-        adminRepository.delete(newAdmin);
-        for (Admin admin : adminRepository.findAll()) {
-            logger.info("---- {} : {}", admin.getId(), admin.getEmail());
-        }
+        logger.info("Admin {} added successfully", newAdmin);
     }
 
 }
