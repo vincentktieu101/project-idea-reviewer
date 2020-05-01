6dd5a9ac1911c058740b0b4a5392a2390a8c2380
Cole Bergmann <43765292+colebergmann@users.noreply.github.com>
2020-04-30 22:51:43 -0700

    cb - broken oauth mocking

---
+++ src/test/java/edu/ucsb/cs48/s20/demo/end2end/UserFlowEnd2EndTest.java
@@ -0,0 +1,81 @@
+package edu.ucsb.cs48.s20.demo.end2end;
+
+import com.github.tomakehurst.wiremock.extension.responsetemplating.ResponseTemplateTransformer;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class UserFlowEnd2EndTest {
+
+    static final String APP_BASE_URL = "http://localhost:8080";
+
+    private WebDriver webDriver;
+
+    @Rule
+    public WireMockRule mockOAuth2Provider = new WireMockRule(wireMockConfig()
+            .port(8077)
+            .extensions(new ResponseTemplateTransformer(true)));
+
+    @Before
+    public void setUp() {
+        webDriver = new ChromeDriver();
+
+        mockOAuth2Provider.stubFor(get(urlPathEqualTo("/favicon.ico")).willReturn(notFound()));
+
+        mockOAuth2Provider.stubFor(get(urlPathMatching("/oauth/authorize?.*"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "text/html")
+                        .withBodyFile("login.html")));
+
+        mockOAuth2Provider.stubFor(post(urlPathEqualTo("/login"))
+                .willReturn(temporaryRedirect("http://localhost:8080/login/oauth2/code/wiremock{{formData request.body 'form' urlDecode=true}}?code={{{randomValue length=30 type='ALPHANUMERIC'}}}&state={{{form.state}}}")));
+
+        mockOAuth2Provider.stubFor(post(urlPathEqualTo("/oauth/token"))
+                .willReturn(okJson("{\"token_type\": \"Bearer\",\"access_token\":\"{{randomValue length=20 type='ALPHANUMERIC'}}\"}")));
+
+        mockOAuth2Provider.stubFor(get(urlPathEqualTo("/userinfo"))
+                .willReturn(okJson("{\"sub\":\"my-id\",\"email\":\"joe@ucsb.edu\", \"hd\":\"ucsb.edu\", \"name\":\"Joe\", \"given_name\":\"Joe\", \"family_name\":\"Gaucho\"}")));
+    }
+
+    @After
+    public void reset() {
+        if (webDriver != null) {
+            webDriver.close();
+        }
+    }
+
+    @Test
+    public void testTest() {
+        System.out.println("This is a good test right here!");
+        webDriver.get(APP_BASE_URL + "/oauth2/authorization/wiremock");
+        System.out.println("                ========== CURRENT URL " + webDriver.getCurrentUrl());
+        assert(webDriver.getCurrentUrl()).startsWith("http://localhost:8077/oauth/authorize");
+
+        webDriver.findElement(By.name("username")).sendKeys("joe@ucsb.edu");
+        webDriver.findElement(By.name("password")).sendKeys("password");
+        webDriver.findElement(By.id("submit")).click();
+
+        String url = webDriver.getCurrentUrl();
+
+        assert(webDriver.getCurrentUrl().startsWith(""));
+
+        assert(true);
+    }
+
+}
