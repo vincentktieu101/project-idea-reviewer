cbd72747e43fdd48a36b08687010a924811c554b
Cole Bergmann <43765292+colebergmann@users.noreply.github.com>
2020-05-01 01:59:39 -0700

    cb - finished end2end test for user flow

--- src/test/java/edu/ucsb/cs48/s20/demo/end2end/UserFlowEnd2EndTest.java
+++ src/test/java/edu/ucsb/cs48/s20/demo/end2end/UserFlowEnd2EndTest.java
@@ -11,17 +11,35 @@ import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlMergeMode;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import javax.transaction.Transactional;
+
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 
+/**
+ * This test runs a complete end-to-end test on the project as a single test.
+ * Starts by logging in as a student, submitting a review, and reviewing 5 ideas
+ *
+ * If you are trying to duplicate this test in a project, make sure to copy the
+ * html template under the test/resources/__files/ directory!
+ *
+ * NOTE: To run this test you must install ChromeDriver on your machine
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-
+@Transactional
 public class UserFlowEnd2EndTest {
+
+    /**
+     * Sets up custom environment variables for this test class. This is the information for our fake oauth provider.
+     */
     static {
         System.setProperty("spring.security.oauth2.client.provider.wiremock.authorization-uri", "http://localhost:8077/oauth/authorize");
         System.setProperty("spring.security.oauth2.client.provider.wiremock.token-uri", "http://localhost:8077/oauth/token");
@@ -35,15 +53,16 @@ public class UserFlowEnd2EndTest {
         System.setProperty("spring.security.oauth2.client.registration.wiremock.clientSecret", "whatever");
     }
 
-    static final String APP_BASE_URL = "http://localhost:8080";
-
     private WebDriver webDriver;
 
+    /**
+     * Sets up the api endpoints for the custom mock oauth provider. We have to do this because we can't run automated
+     * tests with real Google credentials
+     */
     @Rule
     public WireMockRule mockOAuth2Provider = new WireMockRule(wireMockConfig()
             .port(8077)
             .extensions(new ResponseTemplateTransformer(true)));
-
     @Before
     public void setUp() {
         webDriver = new ChromeDriver();
@@ -54,7 +73,7 @@ public class UserFlowEnd2EndTest {
                 .willReturn(aResponse()
                         .withStatus(200)
                         .withHeader("Content-Type", "text/html")
-                        .withBodyFile("login.html")));
+                        .withBodyFile("mocklogin.html")));
 
         mockOAuth2Provider.stubFor(post(urlPathEqualTo("/login"))
                 .willReturn(temporaryRedirect("{{formData request.body 'form' urlDecode=true}}http://localhost:8080/login/oauth2/code/wiremock?code={{{randomValue length=30 type='ALPHANUMERIC'}}}&state={{{form.state}}}")));
@@ -66,6 +85,9 @@ public class UserFlowEnd2EndTest {
                 .willReturn(okJson("{\"sub\":\"my-id\",\"email\":\"joe@ucsb.edu\", \"hd\":\"ucsb.edu\", \"name\":\"Joe\", \"given_name\":\"Joe\", \"family_name\":\"Gaucho\"}")));
     }
 
+    /**
+     * Tear down the webDriver (automated clicker)
+     */
     @After
     public void reset() {
         if (webDriver != null) {
@@ -73,15 +95,37 @@ public class UserFlowEnd2EndTest {
         }
     }
 
+    static String MEDIUM_TEXT = "This is a long bit of text. This will be reused in some text fields throughout the test.";
+
     @Test
     public void testTest() {
-        webDriver.get(APP_BASE_URL + "/oauth2/authorization/wiremock");
+        // Navigate to login page
+        webDriver.get("http://localhost:8080/oauth2/authorization/wiremock");
+        // Make sure Spring redirected us to the right place
         assert(webDriver.getCurrentUrl()).startsWith("http://localhost:8077/oauth/authorize");
-
-        webDriver.findElement(By.name("username")).sendKeys("joe@ucsb.edu");
-        webDriver.findElement(By.name("password")).sendKeys("password");
         webDriver.findElement(By.id("submit")).click();
+        // Verify login was successful (and did not result in /error path)
         assert(webDriver.getCurrentUrl().equalsIgnoreCase("http://localhost:8080/"));
+
+        // fill out idea form
+        webDriver.findElement(By.id("ideaTitle")).sendKeys("This is a title for a fantastic idea");
+        webDriver.findElement(By.id("ideaBody")).sendKeys(MEDIUM_TEXT);
+        webDriver.findElement(By.id("submit")).click();
+
+        // Make sure we got redirected to the review review page (lol)
+        assert(webDriver.getCurrentUrl().equalsIgnoreCase("http://localhost:8080/reviews/perform"));
+
+        // Enter 5 reviews
+        for (int i = 0; i < 5; i++) {
+            assert webDriver.findElement(By.id("reviewsNeeded")).getText().contains(String.valueOf(5-i)); //check user sees x remaining
+            webDriver.findElement(By.id("radio5Label")).click();
+            webDriver.findElement(By.className("form-control")).sendKeys(MEDIUM_TEXT);
+            webDriver.findElement(By.id("submit")).click();
+        }
+
+        // User has inputted 5 reviews. Make sure the reviewsNeeded banner is gone, and the reviewsSufficient banner shows
+        assert(webDriver.findElements(By.id("reviewsNeeded")).isEmpty());
+        assert(webDriver.findElements(By.id("reviewsSufficient")).size() == 1);
     }
 
 }
